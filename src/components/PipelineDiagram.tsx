7c951262a7cfc50f5eebb2d4648caef24cbd58c0
momopro21 <info@monique-provost.com>
2026-04-03 05:08:58 -0400

    Add PipelineDiagram component to display a diagram

--- src/components/PipelineDiagram.tsx
+++ src/components/PipelineDiagram.tsx
@@ -1,1 +1,15 @@
+import React from "react";
 
+const PipelineDiagram = () => {
+  return (
+    <div className="my-10 flex justify-center">
+      <img
+        src="/flowchart_vidame.png"
+        alt="Diagramme pipeline Vidame"
+        className="w-full max-w-5xl"
+      />
+    </div>
+  );
+};
+
+export default PipelineDiagram;

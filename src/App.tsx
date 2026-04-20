e37e838824d0b8958ed52f0e9c5281b12847b65b
momopro21 <info@monique-provost.com>
2026-04-20 14:54:39 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -556,9 +556,7 @@ function Accueil({setPage,lang,}: {setPage: (page: string) => void;lang: "fr" | 
       <p className="mt-6">
         Mais une question demeure :{" "}
         <span className="font-semibold text-slate-900">
-          comment intégrer ces outils sans perdre la maîtrise des données,
-          <br /> 
-          du sens et des exigences éthiques qui encadrent la recherche ?
+          comment intégrer ces outils sans perdre la maîtrise des données, du sens et des exigences éthiques qui encadrent la recherche ?
         </span>
       </p>
       <p className="mt-4">

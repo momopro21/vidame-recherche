1800eb9df0bc8aaee908322efa8a90b4cc5e41f3
momopro21 <info@monique-provost.com>
2026-04-20 19:03:03 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -556,7 +556,7 @@ function Accueil({setPage,lang,}: {setPage: (page: string) => void;lang: "fr" | 
       <p>Les gains sont importants.</p>
       <p>Mais une question demeure.</p>
 
-      <p className="pt-2 font-medium text-slate-700">
+     <p className="pt-2 text-[20px] font-medium leading-7 text-slate-700 md:mx-auto md:max-w-[520px] md:text-center">
         Comment intégrer ces outils sans perdre la maîtrise des données, du sens et des exigences éthiques qui encadrent la recherche ?
     </p>
 

b2e5d949c90dfdaddba7cdc10fb2f801a33a28b8
momopro21 <info@monique-provost.com>
2026-04-27 09:35:14 -0400

    Change 'workflow' to 'pipeline' in language condition

--- src/App.tsx
+++ src/App.tsx
@@ -1421,7 +1421,7 @@ const pipelineSteps = [
         : "border border-slate-300 bg-gradient-to-b from-slate-200 to-slate-300 text-slate-900"
     }`}
   >
-    {lang === "fr" ? "Chaîne opératoire Vidame" : "Vidame workflow"}
+    {lang === "fr" ? "Chaîne opératoire Vidame" : "Vidame pipeline"}
   </button>
 </motion.div>
   </div>

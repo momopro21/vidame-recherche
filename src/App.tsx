05a5b83b76227e9d0c421e2868f4fe3c5cd37ced
momopro21 <info@monique-provost.com>
2026-04-27 11:08:05 -0400

    Fix paragraph tags in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -532,8 +532,7 @@ return (
     </>
   ) : (
     <>
-      <p>
-       <p className="md:mx-auto md:max-w-2xl">
+      <p className="md:mx-auto md:max-w-2xl">
         In the current context of digital transformation in research, artificial intelligence tools are rapidly reshaping qualitative data processing practices.
       </p>
 

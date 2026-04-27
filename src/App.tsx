a313d57da8fa98b73fae66fffc91124203373c6c
momopro21 <info@monique-provost.com>
2026-04-27 11:11:36 -0400

    Update paragraph class to max-w-3xl

--- src/App.tsx
+++ src/App.tsx
@@ -532,7 +532,7 @@ return (
     </>
   ) : (
     <>
-      <p className="md:mx-auto md:max-w-2xl">
+      <p className="md:mx-auto md:max-w-3xl">
         In the current context of digital transformation in research, artificial intelligence tools are rapidly reshaping qualitative data processing practices.
       </p>
 
